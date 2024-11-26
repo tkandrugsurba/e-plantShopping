95c060c137f423b3a027c0dd5f7620884292dc9d
tkandrugsurba <tkandrugsurba@gmail.com>
2024-11-25 23:10:26 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux"; // Import Provider from react-redux
+import store from "./store.js"; // Import the Redux store
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+// Render the App component wrapped with Provider to enable global state access
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
