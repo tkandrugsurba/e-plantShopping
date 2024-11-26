d977a0fbeebe8448e5cfd9f62b557ced4ccbb88b
tkandrugsurba <tkandrugsurba@gmail.com>
2024-11-25 22:48:49 -0500

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingItem = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const item = state.items.find((item) => item.name === name);
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
